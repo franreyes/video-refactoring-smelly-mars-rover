909536e5fa8ef448b267d0b65eafe0e949185979
freyes <fran@codesai.com>
2023-05-10 17:47:08 +0100

    first commit

---
+++ SmellyMarsRover/Rover.cs
@@ -0,0 +1,157 @@
+﻿using System;
+
+namespace SmellyMarsRover
+{
+    public class Rover
+    {
+        private Direction _direction;
+        private Coordinates _coordinates;
+
+        public Rover(int x, int y, string direction)
+        {
+            Direction = direction;
+            SetCoordinates(x, y);
+        }
+
+        private void SetCoordinates(int x, int y)
+        {
+            _coordinates = new Coordinates(x, y);
+        }
+
+        private string Direction
+        {
+            set => _direction = new Direction(value);
+        }
+
+        public void Receive(string commandsSequence)
+        {
+            for (var i = 0; i < commandsSequence.Length; ++i)
+            {
+                var command = commandsSequence.Substring(i, 1);
+
+                if (command.Equals("l"))
+                {
+                    // Rotate Rover to the left
+                    if (IsFacingNorth())
+                    {
+                        Direction = "W";
+                    }
+                    else if (IsFacingSouth())
+                    {
+                        Direction = "E";
+                    }
+                    else if (IsFacingWest())
+                    {
+                        Direction = "S";
+                    }
+                    else
+                    {
+                        Direction = "N";
+                    }
+                }
+                else if (command.Equals("r"))
+                {
+                    // Rotate Rover to the right
+                    if (IsFacingNorth())
+                    {
+                        Direction = "E";
+                    }
+                    else if (IsFacingSouth())
+                    {
+                        Direction = "W";
+                    }
+                    else if (IsFacingWest())
+                    {
+                        Direction = "N";
+                    }
+                    else
+                    {
+                        Direction = "S";
+                    }
+                }
+                else
+                {
+                    // Displace Rover
+                    var displacement1 = -1;
+
+                    if (command.Equals("f"))
+                    {
+                        displacement1 = 1;
+                    }
+
+                    var displacement = displacement1;
+
+                    if (IsFacingNorth())
+                    {
+                        _coordinates = _coordinates.MoveAlongYAxis(displacement);
+                    }
+                    else if (IsFacingSouth())
+                    {
+                        _coordinates = _coordinates.MoveAlongYAxis(-displacement);
+                    }
+                    else if (IsFacingWest())
+                    {
+                        _coordinates = _coordinates.MoveAlongXAxis(-displacement);
+                    }
+                    else
+                    {
+                        _coordinates = _coordinates.MoveAlongXAxis(displacement);
+                    }
+                }
+            }
+        }
+
+        private bool IsFacingWest()
+        {
+            return _direction.Value.Equals("W");
+        }
+
+        private bool IsFacingSouth()
+        {
+            return _direction.Value.Equals("S");
+        }
+
+        private bool IsFacingNorth()
+        {
+            return _direction.Value.Equals("N");
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Rover)obj);
+        }
+
+        protected bool Equals(Rover other)
+        {
+            return Equals(_direction, other._direction) && Equals(_coordinates, other._coordinates);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_direction, _coordinates);
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(_direction)}: {_direction}, {nameof(_coordinates)}: {_coordinates}";
+        }
+    }
+
+    internal record Coordinates(int x, int y)
+    {
+        public Coordinates MoveAlongYAxis(int displacement)
+        {
+            return new Coordinates(this.x, this.y + displacement);
+        }
+
+        public Coordinates MoveAlongXAxis(int displacement)
+        {
+            return new Coordinates(this.x + displacement, this.y);
+        }
+    }
+
+    internal record Direction(string Value);
+}
